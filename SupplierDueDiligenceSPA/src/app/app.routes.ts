f336c5be144f9c78a308bde778eecb632afd2154
NeuronaBens <neuronabens@gmail.com>
2024-01-18 11:07:30 -0500

    feat: removed tests

--- SupplierDueDiligenceSPA/src/app/app.routes.ts
+++ SupplierDueDiligenceSPA/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { DataTableComponent } from './components/data-table/data-table.component';
-import { TestComponent } from './components/test/test.component';
 
 export const routes: Routes = [
   {
@@ -11,9 +10,5 @@ export const routes: Routes = [
   {
     path: 'app',
     component: DataTableComponent,
-  },
-  {
-    path: 'test',
-    component: TestComponent,
   },
 ];
